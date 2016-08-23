c9556bcbb128536f148b175b0e1555cf7468c613
Кеша <keshapudelev@ya.ru>
2016-08-23 04:19:29 +0300

    Created first page Hello whith 'loremstuff'

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello</title>
+</head>
+<body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque blanditiis adipisci qui vitae sed repellat rerum asperiores earum porro, dolore, quasi fuga optio facilis ipsam quia dolor architecto natus placeat.
+	</p>
+</body>
+</html>
