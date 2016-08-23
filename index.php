d583ba5a10099bf82a9fa5b38871ed0897206bd0
Кеша <keshapudelev@ya.ru>
2016-08-23 04:21:52 +0300

    Added H1 title

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Hello</title>
 </head>
 <body>
+	<h1>Best lorem ever!</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque blanditiis adipisci qui vitae sed repellat rerum asperiores earum porro, dolore, quasi fuga optio facilis ipsam quia dolor architecto natus placeat.
 	</p>
 </body>
