35c7aec64a9075fd10bc7887904975c9a5a8a037
bymind <studio@bymind.ru>
2016-08-23 04:28:48 +0300

    Added footer menu

--- index.php
+++ index.php
@@ -8,5 +8,14 @@
 	<h1>Best lorem ever!</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque blanditiis adipisci qui vitae sed repellat rerum asperiores earum porro, dolore, quasi fuga optio facilis ipsam quia dolor architecto natus placeat.
 	</p>
+	<footer>
+	<ul>
+		<li><a href="/1.html">menu item 1</a></li>
+		<li><a href="/2.html">menu item 1</a></li>
+		<li><a href="/3.html">menu item 1</a></li>
+		<li><a href="/4.html">menu item 1</a></li>
+		<li><a href="/5.html">menu item 1</a></li>
+	</ul>
+	</footer>
 </body>
 </html>
